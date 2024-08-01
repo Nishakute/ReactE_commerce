ef8ae13c8cb14ce8cf7d319c91aaecbe1f20b6d7
Nishakute <kuteenisha0405@gmail.com>
2024-08-01 13:12:23 +0530

    travel intail changes

---
+++ travel/src/components/Travellist.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function TravelList(){
+    
+
+
+
+
+
+    return(
+        <div className='container'>
+            <h2>Travel App</h2>
+            <div></div>
+        </div>
+
+    )
+}
+export default TravelList;
