726fbb46caaa3e59228bb8aea8a0662a27ded668
Nishakute <kuteenisha0405@gmail.com>
2024-08-02 16:35:08 +0530

    chnages

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,4 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route,Routes} from 'react-router-dom'
+import TravelList from './components/Travellist';
 
+function App(){
+    return (
+        <>
+        <Routes>
+         <Route path='/customHook' element={<TravelList />}></Route>
+         <Route path='/customHookUser' element={<usertravel />}></Route>
+         </Routes>
+        </>
+
+    )
+}
+
+export default App;
 
