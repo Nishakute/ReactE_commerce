d0cbf3912047b48f2d4abe29f6a7f4dd5f62094d
Nishakute <kuteenisha0405@gmail.com>
2024-08-28 12:20:37 +0530

    frontend productcard

--- frontendapp/src/component/ProductCard.js
+++ frontendapp/src/component/ProductCard.js
@@ -4,13 +4,15 @@ import {Link} from 'react-router-dom';
 const ProductCard=({elmt})=>{
     return(
         <div className='col-md-6 col-lg-3 my-2'>
+           <Link to={'./fetch-product/${elmt.id'} state={{elmt}}>
              <div className="product-card">
-            <img src={elmt.image} />
+            <img src={elmt.image} alt={''}/>
             <h2>{elmt.title}</h2>
             <p className="price">${elmt.price}</p>
             <button className="btn btn-danger"> Add To Cart</button>
-            <Link to={'./fetch-product/${elmt.id'} state={{elmt}}>More Info</Link>
+           More Info
           </div>
+          </Link>
         </div>
 
     )
